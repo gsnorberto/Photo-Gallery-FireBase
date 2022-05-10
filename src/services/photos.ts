dec46bed5b5868f3809be14cabf69b24b31bf066
Gabriel Norberto <sn_gabriel@outlook.com>
2022-05-10 16:11:50 -0300

    Projeto em desenvolvimento

---
+++ src/services/photos.ts
@@ -0,0 +1,11 @@
+import { Photo } from "../types/Photo";
+import { storage } from "../libs/firebase";
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+
+export const getAll = async () => {
+    let list: Photo[] = [];
+
+    
+
+    return list;
+}
